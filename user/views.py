ae1032b0d7a95db74cc52e550b42685880749885
zhtjtcz <18377221@buaa.edu.cn>
2021-05-18 11:01:20 +0800

    post model change

--- user/views.py
+++ user/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
+@csrf_exempt
 def login(request):
 	if request.method == 'POST':
 		username = request.POST.get('username')
