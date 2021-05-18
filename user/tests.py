8b0b6e026b1b5ca824aa214ffb96485c4dcb38a8
zhtjtcz <18377221@buaa.edu.cn>
2021-05-18 20:44:40 +0800

    register and login

--- user/tests.py
+++ user/tests.py
@@ -4,5 +4,5 @@ import pandas
 import openpyxl
 
 # res = requests.post('http://123.57.194.168:8000/login/', data={'username':'AAA', 'password':'111'})
-res = requests.post('http://127.0.0.1:8000/login/', data={'username':'aaa', 'password':'111'})
+res = requests.post('http://127.0.0.1:8000/login/', data={'username':'admin', 'password':'123456'})
 print(res.text)
--- user/views.py
+++ user/views.py
@@ -6,15 +6,56 @@ from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 @csrf_exempt
+def register(request):
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password1 = request.POST.get('password1')
+		password2 = request.POST.get('password2')
+		email = request.POST.get('email')
+		wxid = request.POST.get('wxid')
+		if Main.objects.filter(username=username).exists():
+			result = {'result': 0, 'message': '用户已存在!'}
+		elif Main.objects.filter(email=email).exists():
+			result = {'result': 0, 'message': '邮箱已注册!'}
+		elif password1 != password2:
+			result = {'result': 0, 'message': '两次密码不匹配!'}
+		else:
+			name,edu = email.split('@')
+			if edu != 'buaa.edu.cn':
+				result = {'result': 0, 'message': '邮箱格式不正确!'}
+			else:
+				all = Main.objects.all()
+				count = len(all) + 1
+				new_user = Main()
+				new_user.ID = count
+				new_user.username = username
+				new_user.password = password1
+				new_user.email = email
+				new_user.wxid = wxid
+				new_user.save()
+				result = {'result': 1, 'message': '注册成功!'}
+		return HttpResponse(json.dumps(result), content_type="application/json")
+	else:
+		result = {'result': 0, 'message': '前端炸了!'}
+		return HttpResponse(json.dumps(result), content_type="application/json")
+
+@csrf_exempt
 def login(request):
 	if request.method == 'POST':
 		username = request.POST.get('username')
 		password = request.POST.get('password')
-		if username == 'aaa' and password == '111':
-			result = {'info': 1}
+		if len(username)==0 or len(password)==0:
+			result = {'result': 0, 'message': r'用户名与密码不允许为空!'}
 		else:
-			result = {'info': -1}
+			if Main.objects.filter(username=username).exists() == False:
+				result = {'result': 0, 'message': '用户不存在!'}
+			else:
+				user = Main.objects.get(username=username)
+				if user.password != password:
+					result = {'result': 0, 'message': '密码不正确!'}
+				else:
+					result = {'result': 1, 'message': '登录成功!'}
 		return HttpResponse(json.dumps(result), content_type="application/json")
 	else:
-		result = {'info': -1}
+		result = {'result': 0, 'message': '前端炸了!'}
 		return HttpResponse(json.dumps(result), content_type="application/json")
