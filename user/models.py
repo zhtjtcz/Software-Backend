7fd490b96e04dcd5c24b3501bdc1fdec593a23d3
zhtjtcz <18377221@buaa.edu.cn>
2021-05-23 21:02:35 +0800

    userinfo table

--- user/models.py
+++ user/models.py
@@ -15,3 +15,12 @@ class EmailCode(models.Model):
 class Userheadshot(models.Model):
 	userID = models.IntegerField(primary_key = True)
 	headshot= models.ImageField(blank = True)
+
+class UserInfo(models.Model):
+	userID = models.IntegerField(primary_key = True)
+	sex = models.IntegerField(blank = True)
+	grade = models.IntegerField(blank = True)
+	telephone = models.CharField(max_length=15, blank = True)
+	location = models.IntegerField(blank = True)
+	tradecount = models.IntegerField(blank = True)
+	score = models.FloatField(blank = True)
