14ec5820aafbc67a83099028bd707286c837832e
zhtjtcz <18377221@buaa.edu.cn>
2021-06-01 09:31:59 +0800

    message database

--- message/models.py
+++ message/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Msg(models.Model):
+	ID = models.IntegerField(primary_key = True)
+	objectID = models.IntegerField()
+	type = models.IntegerField()
+	sendID = models.IntegerField()
+	replyID = models.IntegerField()
+	text = models.CharField(max_length = 110)
+	sendtime = models.CharField(max_length = 30)
--- message/views.py
+++ message/views.py
@@ -5,6 +5,25 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from zhenguo.settings import *
 from zhenguo.token import *
+from message.models import *
 import datetime
 import traceback
 # Create your views here.
+
+@csrf_exempt
+def uploadinfo(request):
+	if request.method == 'POST':
+		data_json = json.loads(request.body)
+		token = data_json.get('token')
+		id = Check(token)
+		if id==-1:
+			result = {'result': 0, 'message': 'Token有误!'}
+			return HttpResponse(json.dumps(result), content_type="application/json")
+		msg = Msg(ID=len(Msg.objects.all()), objectID=data_json.get('objectid'), type=data_json.get('type'),
+			sendID=id, replyID=data_json.get('reply'), text=data_json.get('text'), sengtime=str(datetime.datetime.now()))
+		msg.save()
+		result = {'result': 1, 'message': '留言成功!'}
+		return HttpResponse(json.dumps(result), content_type="application/json")
+	else:
+		result = {'result': 0, 'message': '前端炸了!'}
+		return HttpResponse(json.dumps(result), content_type="application/json")
